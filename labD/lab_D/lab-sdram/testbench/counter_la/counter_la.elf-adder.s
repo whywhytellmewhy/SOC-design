86ed3ff7e4063e44b881ed9da308140afbeca4ca
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2024-01-14 04:40:03 -0500

    master to branch push

--- labD/lab_D/lab-sdram/testbench/counter_la/counter_la.elf-adder.s
+++ labD/lab_D/lab-sdram/testbench/counter_la/counter_la.elf-adder.s
@@ -6,7 +6,7 @@
 	.text
 .Ltext0:
 	.cfi_sections	.debug_frame
-	.file 0 "/home/ubuntu/lab_D/lab-sdram/testbench/counter_la" "../../firmware/adder.c"
+	.file 0 "/home/ubuntu/LAB/github_teamsync/SOC-design/labD/lab_D/lab-sdram/testbench/counter_la" "../../firmware/adder.c"
 	.globl	Number
 	.data
 	.align	2
@@ -351,7 +351,7 @@ adder:
 	.string	"unsigned int"
 	.section	.debug_line_str,"MS",@progbits,1
 .LASF1:
-	.string	"/home/ubuntu/lab_D/lab-sdram/testbench/counter_la"
+	.string	"/home/ubuntu/LAB/github_teamsync/SOC-design/labD/lab_D/lab-sdram/testbench/counter_la"
 .LASF0:
 	.string	"../../firmware/adder.c"
 	.ident	"GCC: (g1ea978e3066) 12.1.0"
--- labD/lab_D/lab-sdram/testbench/counter_la/counter_la.elf-counter_la.s
+++ labD/lab_D/lab-sdram/testbench/counter_la/counter_la.elf-counter_la.s
@@ -6,7 +6,7 @@
 	.text
 .Ltext0:
 	.cfi_sections	.debug_frame
-	.file 0 "/home/ubuntu/lab_D/lab-sdram/testbench/counter_la" "counter_la.c"
+	.file 0 "/home/ubuntu/LAB/github_teamsync/SOC-design/labD/lab_D/lab-sdram/testbench/counter_la" "counter_la.c"
 	.align	2
 	.type	flush_cpu_icache, @function
 flush_cpu_icache:
@@ -921,7 +921,7 @@ main:
 	.string	"signed char"
 	.section	.debug_line_str,"MS",@progbits,1
 .LASF1:
-	.string	"/home/ubuntu/lab_D/lab-sdram/testbench/counter_la"
+	.string	"/home/ubuntu/LAB/github_teamsync/SOC-design/labD/lab_D/lab-sdram/testbench/counter_la"
 .LASF0:
 	.string	"counter_la.c"
 	.ident	"GCC: (g1ea978e3066) 12.1.0"
